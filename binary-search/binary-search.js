0251e86232ebddc01307bcc294b3267fed8f5a84
Matt Hemmerly <mhemmerly@gmail.com>
2019-04-05 10:52:56 -0500

    BST done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,28 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if (array.length < 1) return false;
+
+	if (array.length === 1) {
+		if (array[0] === target){
+			return true;
+		}else{
+			return false
+	}
+
+	}else{
+		let midInd =Math.ceil(array.length/2)
+		let midpoint = array[midInd]
+		let leftArr = array.slice(0,midInd);
+		let rightArr = array.slice(midInd+1)
+		if (midpoint === target){
+			return true;
+		}else if (midpoint > target){
+			 return binarySearch(leftArr,target)
+		}else {
+			return binarySearch(rightArr,target)
+		}
+	}
 };
 
 /*
